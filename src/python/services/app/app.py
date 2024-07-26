f96578a16bb5086341e7799752920f6fd3203cdf
raghav-endor <raghav@endor.ai>
2024-07-26 10:51:01 +0530

    used where fucntion of cerifi

--- src/python/services/app/app.py
+++ src/python/services/app/app.py
@@ -1,4 +1,6 @@
-import requests
+import requests 
+import urllib3 
+import certifi
 
 def get_external_ip():
     response = requests.get("https://api.ipify.org?format=json")
@@ -7,6 +9,16 @@ def get_external_ip():
 
 def main():
     print(f"My external IP is: {get_external_ip()}")
+    print(getList("https://google.com"))
+
+def getList(listUrl):
+    http = urllib3.PoolManager(
+        cert_reqs='CERT_REQUIRED',  # Force certificate check.
+        ca_certs=certifi.where(),  # Path to the Certifi bundle.
+    )
+
+    data = http.request('GET', listUrl, timeout=10).data
+    return data
 
 if __name__ == "__main__":
     main()
